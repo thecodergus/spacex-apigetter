d1b6af51c84e0eeb14755b8d7b82de6374921298
gusscamargo <guga.power@hotmail.com>
2022-09-04 22:51:23 -0300

    simplificatioN

--- src/tools/requests.ts
+++ src/tools/requests.ts
@@ -1,9 +1,6 @@
 import {request, gql } from "graphql-request"
 
-const requestQL = async (url: string, query: string) => {
-    return await request(url, gql`${query}`)
-}
-
+const requestQL = async (url: string, query: string) => await request(url, gql`${query}`)
 
 export default {
     requestQL
