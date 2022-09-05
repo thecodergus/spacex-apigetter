1bac236d23a6c6b1cdbe7614778da9cf0dd20b80
gusscamargo <guga.power@hotmail.com>
2022-09-04 23:14:54 -0300

    debug

--- src/tools/processing_data.ts
+++ src/tools/processing_data.ts
@@ -2,12 +2,10 @@ import type { Mission, Ship, FinalData } from "../types"
 
 const process_mission = async (data: Mission[]): Promise<string> => (await data.map((m: Mission) => m.name)).join(",")
 
-const process_ship = async (data: Ship): Promise<FinalData> => {
-    return {
-        ship_name: data.name,
-        missions: await process_mission(data.missions)
-    }
-}
+const process_ship = async (data: Ship): Promise<FinalData> => ({
+    ship_name: data.name,
+    missions: await process_mission(data.missions)
+})
 
 const process_data = async (data: any): Promise<FinalData[]> => await Promise.all(data.ships.filter((s: Ship) => s.active).map(process_ship))
 
